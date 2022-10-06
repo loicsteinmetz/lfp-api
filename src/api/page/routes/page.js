73c46b5969490dd2184b9fea63fb25064d5fda4d
Loïc STEINMETZ <loic.steinmetz@pleiade.app>
2022-10-06 16:45:04 +0200

    First models implementation

--- src/api/page/routes/page.js
+++ src/api/page/routes/page.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * test router
+ * page router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::test.test');
+module.exports = createCoreRouter('api::page.page');
