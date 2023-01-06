cad0f56a0358fae15fb4da1285face951d87c395
Loïc STEINMETZ <loic.steinmetz@pleiade.app>
2023-01-06 15:40:34 +0100

    trad fr

---
+++ src/admin/app.js
@@ -0,0 +1,14 @@
+export default {
+  config: {
+    locales: ['fr', 'en'],
+    translations: {
+      fr: {
+        'app.components.LeftMenu.navbrand.title': 'La Fabrique Populaire',
+      },
+      en: {
+        'app.components.LeftMenu.navbrand.title': 'La Fabrique Populaire',
+      },
+    },
+  },
+  bootstrap() {},
+};
